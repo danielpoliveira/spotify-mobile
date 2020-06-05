742c188b359a1407c73eb451ae18b9c20794c280
danielpolivera <dani.edm@outlook.com>
2020-06-04 22:06:30 -0300

    fixed swipe on time to play 'n' added route params

--- src/pages/playerView/index.js
+++ src/pages/playerView/index.js
@@ -1,15 +1,20 @@
 import React, { useState }                                   from 'react';
-import { View, Text, Image, TouchableOpacity, Animated, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Animated, StyleSheet,Dimensions } from 'react-native';
 import Ionicons                                from 'react-native-vector-icons/Ionicons';
 import MaterialIcons                           from 'react-native-vector-icons/MaterialIcons';
 import MaterialCommunityIcons                  from 'react-native-vector-icons/MaterialCommunityIcons';
-//import Animated                                from 'react-native-reanimated';
+ //import Animated                                from 'react-native-reanimated';
 import { PanGestureHandler }                   from 'react-native-gesture-handler';
 
-export default ({ navigation }) => {
+const WINDOW = Dimensions.get("window");
 
-  const Width     = new Animated.Value(50);
-  const absoluteX = new Animated.Value(0);
+export default ({ navigation, route }) => {
+
+  //console.log(route);
+
+  console.log( WINDOW.width)
+  
+  const absoluteX  = new Animated.Value(0);
 
   const handleGesture = Animated.event([{
     nativeEvent: {
@@ -18,8 +23,10 @@ export default ({ navigation }) => {
 
   }], { useNativeDriver: true,  listener:  
     
-    e => absoluteX.setValue(e.nativeEvent.absoluteX)
-    //e => console.log(e.nativeEvent.absoluteX)
+    e => {
+      absoluteX.setOffset(0),
+      absoluteX.setValue(e.nativeEvent.absoluteX)
+    }
   });
 
 
@@ -43,7 +50,7 @@ return (<View style={{flex:1, backgroundColor: "#3C4A64", paddingHorizontal: 20}
 
     <View style={{flexDirection: "column", alignItems: "center"}}>
       <Text style={{fontSize: 10, fontWeight: '100', color: "#FFFFFF"}} >TOCANDO DA PLAYLIST</Text>
-      <Text style={{fontSize: 12, color: "#FFFFFF", fontWeight: "bold"}} >Daily Mix 1</Text>
+      <Text style={{fontSize: 12, color: "#FFFFFF", fontWeight: "bold"}} >{route.params.playlist_name}</Text>
     </View>
 
     <TouchableOpacity>
@@ -60,30 +67,46 @@ return (<View style={{flex:1, backgroundColor: "#3C4A64", paddingHorizontal: 20}
       elevation: 25
     }}>
 
-    <Image style={{width: 360, height: 360,}} 
-           source={{uri: "https://i.scdn.co/image/ab67616d00001e0226f7709399913201ebe40eee"}} />
+
+    <Image style={{width: 360, height: 360,}} source={{uri: route.params.albumArtUri}} />
     </View>
   </View>
 
   <View style={{paddingHorizontal: 10, paddingVertical: 12.5}}>
-    <Text style={{color: "#FFFFFF", fontSize: 20, fontWeight: "bold"}} >Cthulhu Sleeps</Text>
-    <Text style={{fontSize: 17, color: "#CFCFCF"}} >deadmau5</Text>
+    <Text style={{color: "#FFFFFF", fontSize: 20, fontWeight: "bold"}} >{route.params.music_name}</Text>
+    <Text style={{fontSize: 17, color: "#CFCFCF"}} >{route.params.artist}</Text>
   </View>
 
   <View style={{flexDirection: "column", paddingHorizontal: 10, }}>
-    <PanGestureHandler onGestureEvent={handleGesture} >
+    <PanGestureHandler onGestureEvent={handleGesture}  >
     <Animated.View onTouchEnd={ e => absoluteX.setValue(e.nativeEvent.locationX) }>
 
-      <View style={{width:'100%', height: 3.5,backgroundColor: "#FFFFFF", marginVertical: 10, overflow: 'hidden'}} >
-        <Animated.View 
-          style={{
-            flex:1, backgroundColor: "#333333ff", 
-            justifyContent: "center",
-            transform: [{ translateX: absoluteX }] 
-          }} 
+      <View style={{width:'100%', height: 3.5,backgroundColor: "#FFFFFF", justifyContent: "center", marginVertical: 10}} >
+
+        <View style={{flex:1, overflow: "hidden"}}>
+          <Animated.View 
+            style={{
+              flex:1, backgroundColor: "#333333ff", 
+              justifyContent: "center",
+              transform: [{ translateX: absoluteX.interpolate({
+                inputRange:  [0, WINDOW.width - 60 ],
+                outputRange: [0, WINDOW.width - 60 ],
+                extrapolate: "clamp",
+              }) }] 
+            }} 
+          >
+          </Animated.View>
+        </View>
+        <Animated.View style={{backgroundColor: "#FFF", width: 10, height: 10, borderRadius:100, position: "absolute",     
+          transform: [{ translateX: absoluteX.interpolate({
+            inputRange:  [0, WINDOW.width - 70 ],
+            outputRange: [0, WINDOW.width - 70 ],
+            extrapolate: "clamp"}) 
+          }]}} 
         />
 
       </View>
+
       
     </Animated.View>
       </PanGestureHandler>
