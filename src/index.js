0af4b3b5b34693be60d69b8cd4e712a302a4d46d
danielpolivera <dani.edm@outlook.com>
2020-06-06 20:32:02 -0300

    added status bar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React               from 'react';
-import { View, StyleSheet} from "react-native";
+import { View, StyleSheet, StatusBar} from "react-native";
 
 import Routes              from './pages/routes';
 
 export default () => 
 <View style={styles.appContainer}>
+  <StatusBar backgroundColor="#121212" />
   <Routes />
 </View>
 
