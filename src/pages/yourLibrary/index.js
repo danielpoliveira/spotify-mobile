ce57e008df7ddced04101a81ad973f4d119706d6
danielpolivera <dani.edm@outlook.com>
2020-06-06 20:32:36 -0300

    added new features

--- src/pages/yourLibrary/index.js
+++ src/pages/yourLibrary/index.js
@@ -1,146 +1,77 @@
-import React          from 'react';
-import { View, Text } from 'react-native';
+import React                             from 'react';
+import Animated                          from 'react-native-reanimated';
+import { View }                          from 'react-native';
+import { TouchableOpacity }              from 'react-native-gesture-handler';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-import { createMaterialTopTabNavigator, MaterialTopTabBar } from '@react-navigation/material-top-tabs';
-import { FlatList } from 'react-native-gesture-handler';
+import Music                             from './music';
+import Podcast                           from './podcast';
 
 const Tab = createMaterialTopTabNavigator();
 
+const MyTabBar = ({ state, descriptors, navigation, position, labelStyle }) => {
+  return (
+    <View style={{ flexDirection: 'row', marginTop: 28 }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
 
-const PLAYLIST_DATA = [
-  {id: "00", title: 'Criar Playlist',                               from: ''},
-  {id: "01", title: 'Músicas Curtidas',                             from: '89 songs'},
-  {id: "02", title: 'Lofi hip hop music - beats to relax/study to', from: 'chilledcow'},
-  {id: "03", title: 'Ski Mask The Slump God Rádio'                , from: 'spotify'},
-  {id: "04", title: 'Lofizinho engcomp',                            from: 'Juliscreudo'},
-  {id: "05", title: '',                                             from: 'famspotify123'},
-  {id: "06", title: '',                                             from: 'Wolfy'},
-  {id: "07", title: '',                                             from: 'Diavlonegro'},
-  {id: "08", title: 'Curtidas na Rádio',                            from: 'Juliscreudo'},
-  {id: "09", title: 'DATA WING Soundtrack',                         from: 'Dan Vogt'},
-  {id: "10", title: 'My songs offline',                             from: 'Juliscreudo'},
-  {id: "11", title: 'NFS Carbon SoundTrack',                        from: 'Ricards Kaupuzs'},
-  {id: "12", title: 'Ouvir com calma depois',                       from: 'Juliscreudo'},
-];
+        const isFocused = state.index === index;
 
-const ARTIST_DATA = [
-  {id: '00', name: 'Amon Tobin'},
-  {id: '01', name: 'Aphex Twin'},
-  {id: '02', name: 'Earl Sweatshirt'},
-  {id: '03', name: 'Frank Ocean'},
-  {id: '04', name: 'jhfly'},
-  {id: '05', name: 'Knxwledge'},
-  {id: '06', name: 'Lil Yachty'},
-  {id: '07', name: 'Night Lovell'},
-  {id: '08', name: 'Playboi Carti'},
-  {id: '09', name: 'Tyler, The Creator'},
-  {id: '10', name: 'Adicionar artistas'},
-];
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
 
-const ALBUM_DATA = [
-  {id: "00", album: "It's True", artist: "Axwell"},
-  {id: "01", album: "FEET OF CLAY", artist: "Earl Sweatshirt"},
-  {id: "02", album: "Blonde", artist: "Frank Ocean"},
-  {id: "03", album: "In My Room", artist: "Frank Ocean"},
-  {id: "04", album: "STOKELEY", artist: "Ski Mask The Slump God"},
-  
-];
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
 
-const Playlist = props => {
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
 
-  return <View style={{ flexDirection: "row", width: "100%", margin: 10}} >
-    <View style={{width: 80,height: 80, backgroundColor: "gray"}} />
-    <View style={{flex:1,flexDirection: "column", backgroundColor: "red", 
-      justifyContent: "center",
-      paddingLeft: 10
-    }} >
-        <Text style={{color: "#FFFFFF", fontSize: 16,fontWeight: "bold"}} >{props.item.title}</Text>
-        <Text style={{color: "#FFFFFF", fontSize: 14}} >{props.item.from}</Text>
+        const inputRange = state.routes.map((_, i) => i);
+        const opacity = Animated.interpolate(position, {
+          inputRange,
+          outputRange:  inputRange.map(i => (i === index ? 1 : 0.5)),
+        });
+
+        return (
+          <TouchableOpacity
+            key={index}
+            accessibilityRole="button"
+            accessibilityStates={isFocused ? ['selected'] : []}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ marginRight: 20 }}
+          >
+            <Animated.Text style={{ opacity, fontSize: 30 ,color: "#FFF", fontWeight: "bold"}}>
+              {label}
+            </Animated.Text>
+          </TouchableOpacity>
+        );
+      })}
     </View>
-
-</View>
+  );
 }
 
-const Playlists = () => 
-<View style={{}} >
-
-  <FlatList 
-  
-  data={PLAYLIST_DATA}
-  keyExtractor={item => item.id}
-  renderItem={item => Playlist(item)}
-
-
-  />
-
-</View>
-
-const Artist = (props) =>(
-  <View style={{ flexDirection: "row", width: "100%", margin: 10}} >
-  <View style={{width: 80,height: 80, backgroundColor: "gray"}} />
-  <View style={{flex:1,flexDirection: "column", backgroundColor: "red", 
-    justifyContent: "center",
-    paddingLeft: 10
-  }} >
-      <Text style={{color: "#FFFFFF", fontSize: 16,fontWeight: "bold"}} >{props.item.name}</Text>
-      
-  </View>
-
-</View> 
-)
-
-const Artists = () => 
-<View>
-  <FlatList  
-    data={ARTIST_DATA} 
-    keyExtractor={item => item.id}
-    renderItem={item => Artist(item)}
-  />
-</View>
-
-
-const Album = props => {
-
-  return <View style={{ flexDirection: "row", width: "100%", margin: 10}} >
-    <View style={{width: 80,height: 80, backgroundColor: "gray"}} />
-    <View style={{flex:1,flexDirection: "column", backgroundColor: "red", 
-      justifyContent: "center",
-      paddingLeft: 10
-    }} >
-        <Text style={{color: "#FFFFFF", fontSize: 16,fontWeight: "bold"}} >{props.item.album}</Text>
-        <Text style={{color: "#FFFFFF", fontSize: 14}} >{props.item.artist}</Text>
-    </View>
-
-</View>
-}
-
-
-const Albums = () => 
-<View>
-  <FlatList  
-    data={ALBUM_DATA} 
-    keyExtractor={item => item.id}
-    renderItem={item => Album(item)}
-  />
-</View>
-
-
-const MyTabBar = ({ state }) => {
- return <View style={{flexDirection: "column", alignItems: "flex-start"}}>
-    <Text style={{
-      borderBottomWidth: 2, borderBottomEndRadius: 100, borderBottomColor: "#31ac5e", 
-      color: "#FFFFFF", fontSize: 17, paddingVertical: 5
-    }} >
-      {state.routeNames[state.index]}</Text>
-</View>
-
-
-}
 export default () => (
-  <Tab.Navigator  tabBar={MyTabBar} >
-    <Tab.Screen  name="Playlists" component={Playlists} />
-    <Tab.Screen  name="Artists" component={Artists} />
-    <Tab.Screen  name="Albums" component={Albums} />
+  <Tab.Navigator tabBar={props => <MyTabBar {...props} />} >
+    <Tab.Screen name="Músicas"  component={Music} />
+    <Tab.Screen name="Podcasts" component={Podcast} />
   </Tab.Navigator>
 );
-
