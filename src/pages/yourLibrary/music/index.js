08b24c677695188d5a44da750f25786bb59a448c
danielpolivera <dani.edm@outlook.com>
2020-06-07 19:35:56 -0300

    added new features 'n' conected music page to api

--- src/pages/yourLibrary/music/index.js
+++ src/pages/yourLibrary/music/index.js
@@ -1,85 +1,141 @@
-import React                             from 'react';
-import { View, Text }                    from 'react-native';
+import React, { useState, useEffect }    from 'react';
+import { View, Text, Image, YellowBox }  from 'react-native';
 import { FlatList, }                     from 'react-native-gesture-handler';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Ionicons                          from 'react-native-vector-icons/Ionicons';
 
 import MyTabBar                          from '../../../components/myTopTabBar'
+import api                               from '../../../services/api';
+
+YellowBox.ignoreWarnings([ 'Each child in a list should have a unique "key" prop.'  ])
 
 const Tab = createMaterialTopTabNavigator();
 
-const PLAYLIST_DATA = [
-  {id: "00", title: 'Criar Playlist',                               from: ''},
-  {id: "01", title: 'Músicas Curtidas',                             from: '89 songs'},
-  {id: "02", title: 'Lofi hip hop music - beats to relax/study to', from: 'chilledcow'},
-  {id: "03", title: 'Ski Mask The Slump God Rádio'                , from: 'spotify'},
-  {id: "04", title: 'Lofizinho engcomp',                            from: 'Juliscreudo'},
-  {id: "05", title: '',                                             from: 'famspotify123'},
-  {id: "06", title: '',                                             from: 'Wolfy'},
-  {id: "07", title: '',                                             from: 'Diavlonegro'},
-  {id: "08", title: 'Curtidas na Rádio',                            from: 'Juliscreudo'},
-  {id: "09", title: 'DATA WING Soundtrack',                         from: 'Dan Vogt'},
-  {id: "10", title: 'My songs offline',                             from: 'Juliscreudo'},
-  {id: "11", title: 'NFS Carbon SoundTrack',                        from: 'Ricards Kaupuzs'},
-  {id: "12", title: 'Ouvir com calma depois',                       from: 'Juliscreudo'},
-];
+const SubtitleIcon = ({ type }) =>
+type==="playlist"?
+<>
+  <View style={{width: 17,height: 12, borderRadius:2, marginRight: 5,backgroundColor: "#CCC", alignItems: "center", justifyContent: "center"}}>
+      <Ionicons name="ios-shuffle" size={15} color="#222"/>
+  </View>
 
-const ARTIST_DATA = [
-  {id: '00', name: 'Amon Tobin'},
-  {id: '01', name: 'Aphex Twin'},
-  {id: '02', name: 'Earl Sweatshirt'},
-  {id: '03', name: 'Frank Ocean'},
-  {id: '04', name: 'jhfly'},
-  {id: '05', name: 'Knxwledge'},
-  {id: '06', name: 'Lil Yachty'},
-  {id: '07', name: 'Night Lovell'},
-  {id: '08', name: 'Playboi Carti'},
-  {id: '09', name: 'Tyler, The Creator'},
-  {id: '10', name: 'Adicionar artistas'},
-];
+  <Text style={{color: "#FFFFFF", fontSize: 12}} >de </Text>
+</>
+: null
 
-const ALBUM_DATA = [
-  {id: "00", album: "It's True", artist: "Axwell"},
-  {id: "01", album: "FEET OF CLAY", artist: "Earl Sweatshirt"},
-  {id: "02", album: "Blonde", artist: "Frank Ocean"},
-  {id: "03", album: "In My Room", artist: "Frank Ocean"},
-  {id: "04", album: "STOKELEY", artist: "Ski Mask The Slump God"},
-];
 
-const ListComponent = (props) => {
-  const title    = props.item.title || ( props.item.name   || props.item.album) || '';
-  const subtitle = props.item.from  || ( props.item.artist || '' );
-                    
+const ListComponent = props => {
+  const { type } = props;
+  
+  const image_uri = 
+  type === 'playlist' || type === 'artist'?  props.item.images[0].url:
+  type === 'album'?                          props.item.album.images[0].url:
+  null
+
+  const title = 
+  type === 'playlist' || type === 'artist' ? props.item.name:
+  type === 'album'?                          props.item.album.name:
+  null;
+  
+  const subtitle = 
+  type === 'playlist'?                       props.item.owner.display_name: 
+  type === 'album?'  ?                       props.item.album.artists[0].name: 
+  null;
+
   return (
     <View style={{ flexDirection: "row", width: "100%", marginBottom: 16,}} >
-      <View style={[ props.isArtist? { borderRadius: 100 } : null, { width: 64,height: 64, backgroundColor: "gray" }]} />
-      <View style={{flex:1,flexDirection: "column", backgroundColor: "red", justifyContent: "center", paddingLeft: 10 }} >
+      <Image source={{uri: image_uri}} style={[ props.isArtist? { borderRadius: 100 } : null, { width: 64,height: 64, backgroundColor: "gray" }]} />
+      
+      <View style={{flex:1,flexDirection: "column", justifyContent: "center", paddingLeft: 10 }} >
         <Text style={{color: "#FFFFFF", fontSize: 16,fontWeight: "bold"}} >{title}</Text>
-        <Text style={{color: "#FFFFFF", fontSize: 14}} >{subtitle}</Text>
+
+        { 
+          type !== 'artist' &&
+
+          <View style={{flexDirection: "row", alignItems: "center"}} >
+            <SubtitleIcon type={type} />        
+            <Text style={{color: "#FFFFFF", fontSize: 14}} >{subtitle}</Text> 
+          </View>
+        }
+
       </View>
     </View>
   );
 }
 
-const Playlists = () => (
-<View>
-  <FlatList data={PLAYLIST_DATA} keyExtractor={item => item.id} renderItem={item => < ListComponent {...item} />} />
-</View>
-);
+const Playlists = () => {
+  const [playlists, setPlaylists] = useState([]);
 
-const Artists = () => 
-<View>
-  <FlatList data={ARTIST_DATA} keyExtractor={item => item.id} renderItem={item => <ListComponent {...item} isArtist />}/>
-</View>
+  useEffect(() => {
+    const loadPlaylists = async () => {
+      const res = await api.get("/me/playlists");
 
-const Albums = () => 
-<View>
-  <FlatList data={ALBUM_DATA} keyExtractor={item => item.id} renderItem={item => <ListComponent {...item} />} />
-</View>
+      const { items } = res.data;
+
+      if(items.length)
+        setPlaylists(items);
+    }
+
+    loadPlaylists();
+  }, []);
+
+  return (
+    <View>
+      <FlatList disableScrollViewPanResponder data={playlists} keyExtractor={item => item.id} renderItem={item => < ListComponent {...item} type="playlist" />} />
+    </View>
+  );
+
+}
+
+const Albums = () => {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    const loadAlbums = async () => {
+      const res = await api.get("/me/albums");
+
+      const { items } = res.data;
+
+      if(items.length)
+        setAlbums(items);
+    }
+
+    loadAlbums();
+  }, []);
+
+  return (
+    <View>
+      <FlatList data={albums} keyExtractor={item => item.id} renderItem={item => <ListComponent {...item} type="album" />} />
+    </View>
+  );
+}
+
+const Artists = () => {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    const loadArtists = async () => {
+      const res = await api.get("/me/following", {params: {type: "artist"} } )
+
+      const { artists } = res.data;
+
+      if(artists.items  && artists.items.length)
+        setArtists(artists.items);
+    }
+
+    loadArtists();
+  }, []);
+
+  return (
+    <View>
+      <FlatList data={artists} keyExtractor={item => item.id} renderItem={item => <ListComponent {...item} isArtist  type="artist"/>}/>
+    </View>
+  );
+}
 
 export default () => (
   <Tab.Navigator tabBar={MyTabBar} >
     <Tab.Screen  name="Playlists" component={Playlists} />
-    <Tab.Screen  name="Artists" component={Artists} />
-    <Tab.Screen  name="Albums" component={Albums} />
+    <Tab.Screen  name="Artists"   component={Artists}   />
+    <Tab.Screen  name="Albums"    component={Albums}    />
   </Tab.Navigator>
 );
