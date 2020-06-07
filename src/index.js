b9217a249820f541a466f38e9278394e3a8e06dc
danielpolivera <dani.edm@outlook.com>
2020-06-07 14:34:11 -0300

    added redux base configurations on project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React               from 'react';
+import React                          from 'react';
 import { View, StyleSheet, StatusBar} from "react-native";
 
-import Routes              from './pages/routes';
+import { Provider }                   from 'react-redux';
+import { createStore } from 'redux';
+
+import Routes                         from './pages/routes';
+
+import reducers from './main/reducers';
+
+const store = createStore(reducers);
 
 export default () => 
-<View style={styles.appContainer}>
-  <StatusBar backgroundColor="#121212" />
-  <Routes />
-</View>
+
+<Provider store={store}>
+  <View style={styles.appContainer}>
+    <StatusBar backgroundColor="#121212" />
+    <Routes />
+  </View>
+</Provider>
 
 const styles  = StyleSheet.create({
   appContainer: { 
