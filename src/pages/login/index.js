175daed29e0a54291a53bc4269e8d0fbf59b51bd
danielpolivera <dani.edm@outlook.com>
2020-06-08 18:57:15 -0300

    created login page

---
+++ src/pages/login/index.js
@@ -0,0 +1,58 @@
+import React                       from 'react';
+import WebView                     from 'react-native-webview'; 
+import axios                       from 'axios';
+import { Buffer }                  from 'buffer';
+
+import { onSignIn }                from '../../services/auth';
+
+import { client_id, secret_id }    from '../../config/auth.json';
+import { scopes, url_login }       from '../../config/config.json';
+import { 
+  redirect_uri, 
+  setParams, 
+  useXURLEncoded 
+}                                  from '../../config/headers';
+
+let logged = false;
+
+const Login = ({ navigation }) => {
+  const getParams = async url => {
+    const code = /code=([^&]+)/.exec(url)[1]; 
+    
+    if(logged == false && code){
+      logged = true;
+      console.log('valor de logged: ----> ', logged);
+
+      const params  = setParams(code);
+      const access  = 'Basic ' + new Buffer(client_id +':'+ secret_id).toString('base64');
+
+      await axios.post(`${url_login}/api/token`, params, { 
+        headers: {
+          'Authorization': access,
+          'Content-Type' : useXURLEncoded,
+         }
+      }).then(async res => {
+        const { access_token } = res.data
+        try {
+          access_token && 
+            await onSignIn(access_token).then(() => navigation.navigate('Tabs'));
+        } catch(err){
+          console.log('erro ===> ', err);
+        }
+
+      }).catch( error => {
+        console.log('aqui', error.response);
+      })
+      
+    }
+  }
+
+  return (
+    <WebView 
+      onNavigationStateChange={e => getParams(e.url)} 
+      source={{ uri: `${url_login}/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}` }} 
+    />
+  )
+}
+
+export default Login;
