90b059441b4a91374f6fc9ddb427246e2478d66e
danielpolivera <dani.edm@outlook.com>
2020-06-08 18:56:55 -0300

    added interceptors to use token on requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,29 @@
-import axios from 'axios';
-
-const TOKEN = "BQB1B0EJ7-rclJqkCZj54S-r_2Ih_Egq21dO7iLnw_MXsqc4P7v8z1oSAXN5ia85Lkq2mDtev5bHx_M0nW6rWFhaAW-ot5VMk_0OFV3NqX-TqbRLhdJsNwPooM3Qx2RNfiM0_1ATnCKs3m_moMON7ClyZdZ3AxkG8EaQEI-6bH8fWPuosMOmuOmCISYmxPtku65Eq14tFiu3DfNuLWwUcHoVU9UNVVfqU5aD8hCKtoMKE8nbQtyM7akdoISTOAI0aeOp9iFhPaxerbTqBySFFOkbg9yCMcjy";
+import axios        from 'axios';
+import { isLogged } from './auth';
 
 const api = axios.create({
   baseURL: 'https://api.spotify.com/v1/',
   timeout: 10000,
-  headers: { Authorization: `Bearer ${TOKEN}`}
 });
+
+api.interceptors.request.use(
+  async config => {
+    return await isLogged()
+    .then(data => {
+      if(data)
+        config.headers.Authorization = `Bearer ${data}`;
+        
+      return Promise.resolve(config);  
+    })
+    .catch(err => {
+      console.log('error: => ', err);
+      return Promise.resolve(config);
+    });
+  },
+  err => {
+    console.log(err);
+    return Promise.reject(err);
+  }
+);
 
 export default api;
