af20064f3f044b7ec1eee4ddaeda8861e8bb58de
danielpolivera <dani.edm@outlook.com>
2020-06-14 15:50:37 -0300

    Custom package with methods for control the spotify using native modules

--- android/app/src/main/java/com/spotifyrn/MainActivity.java
+++ android/app/src/main/java/com/spotifyrn/MainActivity.java
@@ -1,97 +1,27 @@
 package com.spotifyrn;
 
-import com.facebook.react.ReactActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
-//import android.os.Bundle;
-
-//import android.util.Log;
-
-import com.facebook.react.ReactActivityDelegate;                           // Biblioteca necessária para o react-navigation
-import com.facebook.react.ReactRootView;                                   // Biblioteca necessária para o react-navigation
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView; // Biblioteca necessária para o react-navigation
-
-// import com.spotify.android.appremote.api.ConnectionParams;
-// import com.spotify.android.appremote.api.Connector;
-// import com.spotify.android.appremote.api.SpotifyAppRemote;
-
-// import com.spotify.protocol.client.Subscription;
-// import com.spotify.protocol.types.PlayerState;
-// import com.spotify.protocol.types.Track;
-
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;                           // Necessário para o react-navigation
+import com.facebook.react.ReactRootView;                                   // Necessário para o react-navigation
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView; // Necessário para o react-navigation
 
 public class MainActivity extends ReactActivity {
-
-  // private static final String CLIENT_ID = "88411d7a4c714d84aba16fa0fdf18312";
-  // private static final String REDIRECT_URI = "https://example.com/callback";
-  // private SpotifyAppRemote mSpotifyAppRemote;
 
   @Override
   protected String getMainComponentName() {
     return "spotifyRN";
   }
 
-  @Override                                                           // Necessário para o react-navigation
-  protected ReactActivityDelegate createReactActivityDelegate() {     // Necessário para o react-navigation
-    return new ReactActivityDelegate(this, getMainComponentName()) {  // Necessário para o react-navigation
-      @Override                                                       // Necessário para o react-navigation
-      protected ReactRootView createRootView() {                      // Necessário para o react-navigation
-       return new RNGestureHandlerEnabledRootView(MainActivity.this); // Necessário para o react-navigation
+  @Override                                                                 // Necessário para o react-navigation
+  protected ReactActivityDelegate createReactActivityDelegate() {           // Necessário para o react-navigation
+    return new ReactActivityDelegate(this, getMainComponentName()) {        // Necessário para o react-navigation
+      @Override                                                             // Necessário para o react-navigation
+      protected ReactRootView createRootView() {                            // Necessário para o react-navigation
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);       // Necessário para o react-navigation
       }
     };
   }
   
-  /*protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-    //   setContentView(R.layout.activity_main);
-  }*/
-
-  /*@Override
-  protected void onStart() {
-      super.onStart();
-      ConnectionParams connectionParams =
-              new ConnectionParams.Builder(CLIENT_ID)
-                      .setRedirectUri(REDIRECT_URI)
-                      .showAuthView(true)
-                      .build();
-
-      SpotifyAppRemote.connect(this, connectionParams,
-              new Connector.ConnectionListener() {
-
-                  public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                      mSpotifyAppRemote = spotifyAppRemote;
-                      Log.d("MainActivity", "Connected! Yay!");
-
-                      // Now you can start interacting with App Remote
-                      connected();
-                  }
-
-                  public void onFailure(Throwable throwable) {
-                      Log.e("MyActivity", throwable.getMessage(), throwable);
-
-                      // Something went wrong when attempting to connect! Handle errors here
-                  }
-              });
-  }
-
-  @Override
-  protected void onStop() {
-      super.onStop();
-      SpotifyAppRemote.disconnect(mSpotifyAppRemote);
-  }
-
-  private void connected() {
-      // Play a playlist
-      mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
-
-      // Subscribe to PlayerState
-      mSpotifyAppRemote.getPlayerApi()
-              .subscribeToPlayerState()
-              .setEventCallback(playerState -> {
-                  final Track track = playerState.track;
-                  if (track != null) {
-                      Log.d("MainActivity", track.name + " by " + track.artist.name);
-                  }
-              });
-  }*/
 }
---
+++ android/app/src/main/java/com/spotifyrn/SpotifyModule.java
@@ -0,0 +1,196 @@
+package com.spotifyrn;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.uimanager.IllegalViewOperationException;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+
+public class SpotifyModule extends ReactContextBaseJavaModule {
+  private static ReactApplicationContext reactContext;
+
+  private static final String CLIENT_ID      =  "88411d7a4c714d84aba16fa0fdf18312";
+  private static final String REDIRECT_URI   = "https://example.com/callback";
+  private static final String E_LAYOUT_ERROR = "E_LAYOUT_ERROR";
+  
+  private SpotifyAppRemote mSpotifyAppRemote;
+  
+  private boolean play    = false;
+  private boolean connect = false;
+
+  SpotifyModule(ReactApplicationContext context){
+    super(context);
+    reactContext = context;
+  }
+
+  public boolean getConnect(){
+    return this.connect;
+  }
+
+  public void setConnect(boolean value){
+    this.connect = value;
+  }
+
+  public boolean getPlay() {
+    return this.play;
+  }
+
+  public void setPlay(boolean value) {
+    this.play = value;
+  }
+
+  @Override
+  public String getName() {
+    return "SpotifySDK";
+  }
+
+  public void startConnection() {
+    ConnectionParams connectionParams = new ConnectionParams
+                                                    .Builder(CLIENT_ID)
+                                                    .setRedirectUri(REDIRECT_URI)
+                                                    .showAuthView(true)
+                                                    .build();
+    
+    SpotifyAppRemote.connect(super.getReactApplicationContext(), connectionParams, 
+      new Connector.ConnectionListener() {
+            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                mSpotifyAppRemote = spotifyAppRemote;      
+                setConnect(true);
+            }
+
+            public void onFailure(Throwable throwable) {
+                setConnect(false);
+                //Log.e("MyActivity", throwable.getMessage(), throwable);
+            }
+      });
+  }
+
+  @ReactMethod
+  public void onStart(Promise promise){
+    if(connect) return;
+
+    ConnectionParams connectionParams = new ConnectionParams
+                                                    .Builder(CLIENT_ID)
+                                                    .setRedirectUri(REDIRECT_URI)
+                                                    .showAuthView(true)
+                                                    .build();
+    
+    SpotifyAppRemote.connect(super.getReactApplicationContext(), connectionParams, new Connector.ConnectionListener() {
+            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                mSpotifyAppRemote = spotifyAppRemote;      
+                setConnect(true);
+                getTrack(promise);
+            }
+
+            public void onFailure(Throwable throwable) {
+                setConnect(false);
+                promise.reject(E_LAYOUT_ERROR, throwable.getMessage());      
+            }
+    });
+  }
+
+  @ReactMethod
+  public void getTrack(Promise promise){
+    try {
+      WritableMap map = Arguments.createMap();
+      
+      if(mSpotifyAppRemote == null || !mSpotifyAppRemote.isConnected()){
+        startConnection();
+        promise.reject(E_LAYOUT_ERROR, "Error on getting music. Try again.");      
+      }
+      
+      else
+      mSpotifyAppRemote.getPlayerApi()
+                       .subscribeToPlayerState()
+                       .setEventCallback(playerState -> {
+                        final Track track = playerState.track;
+
+                        if(track != null) {
+                          map.putBoolean("is_podcast", track.isPodcast);    
+                          map.putBoolean("is_episode", track.isEpisode);    
+                          map.putString("image_uri",   track.imageUri.raw.split("spotify:image:")[1]);
+                          map.putString("artist_name", track.artist.name);
+                          map.putString("artist_uri",  track.artist.uri);
+                          map.putString("album_name",  track.album.name);
+                          map.putString("album_uri",   track.album.uri);
+                          map.putDouble("duration",    track.duration);
+                          map.putString("track_name",  track.name);
+                          map.putString("track_uri",   track.uri);
+
+                          promise.resolve(map);
+                        }
+      });
+
+    } catch (IllegalViewOperationException e) {
+      promise.reject(E_LAYOUT_ERROR, e);
+    }
+  }
+
+  @ReactMethod
+  public void onStop() {
+    if(getConnect()){
+      setConnect(false);
+      setPlay(false);
+
+      mSpotifyAppRemote.getPlayerApi().pause();
+      SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+  }
+
+  @ReactMethod
+  public boolean onPlayPause(){
+    if(getConnect()){
+        if(getPlay())
+            mSpotifyAppRemote.getPlayerApi().pause();
+        else
+            mSpotifyAppRemote.getPlayerApi().resume();
+        
+        setPlay(!getPlay());
+    } else{
+       startConnection();
+    }
+
+    return true;
+  }
+
+  @ReactMethod
+  public void onNext(){
+    if(!getConnect()){
+      startConnection();
+    }
+    
+    if(!getPlay()) setPlay(true);
+
+    mSpotifyAppRemote.getPlayerApi().skipNext();
+  }
+  
+  @ReactMethod
+  public void onPrev(){
+    if(!getConnect()){
+      startConnection();
+    }
+
+    mSpotifyAppRemote.getPlayerApi().skipPrevious();
+  }
+    
+}
